919a118971b267c9665906ae03ff0c0755115000
Myriam <92749914+MyriamTazi@users.noreply.github.com>
2021-12-03 12:53:51 +0000

    Create Inheritence.py

---
+++ Inheritence.py
@@ -0,0 +1,6 @@
+""" 
+Inheritance makes it possible for a class to literally 'inherit' all the methods and properties from another class.
+
+We call the original class a PARENT or BASE class.
+
+The subsequent calls is called CHILD or DERIVED class. """
